3cd55bde52caf691a5e4488e2838834c9f11c0e7
JinchengHeRyan <delteforce@outlook.com>
2024-12-03 15:16:11 -0800

    Add loss_plot

--- loss_plot.py
+++ loss_plot.py
@@ -5,27 +5,33 @@ def get_loss(log_file):
     with open(log_file, "r") as f:
         lines = f.readlines()
 
-    retain_train_loss = list()
-    forget_train_loss = list()
+    total_loss = list()
+    retain_loss = list()
+    forget_loss = list()
 
     for line in lines:
         if "[Train Summary]" in line:
-            retain_train_loss.append(
+            total_loss.append(
+                float(line[line.index("Total Loss:") + 11 : line.index("Retain Loss:")])
+            )
+            retain_loss.append(
                 float(
                     line[line.index("Retain Loss:") + 12 : line.index("Forget Loss:")]
                 )
             )
-            forget_train_loss.append(float(line[line.index("Forget Loss:") + 12 :]))
-    return retain_train_loss, forget_train_loss
+            forget_loss.append(float(line[line.index("Forget Loss:") + 12 :]))
+    return total_loss, retain_loss, forget_loss
 
 
 if __name__ == "__main__":
-    log_file = "./logs/experiment_1B/2024-12-02-2316.log"
+    log_file = "./logs/experiment_1B_mix_dynamic_0.5/2024-12-03-1437.log"
 
-    retain_train_loss, forget_train_loss = get_loss(log_file)
+    total_loss, retain_train_loss, forget_train_loss = get_loss(log_file)
+    print(total_loss)
     print(retain_train_loss)
     print(forget_train_loss)
 
+    plt.plot(total_loss, "-o", label="Total Train Loss")
     plt.plot(retain_train_loss, "-o", label="Retain Train Loss")
     plt.plot(forget_train_loss, "-o", label="Forget Train Loss")
     plt.title("Training Loss")
